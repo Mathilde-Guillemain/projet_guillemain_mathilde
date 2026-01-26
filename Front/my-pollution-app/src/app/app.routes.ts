8f0f84db55ded129dd01a65335837e4678c01451
Mathilde <maguillemain@unistra.fr>
2026-01-26 23:44:09 +0100

    Suppression user-list et user-form

--- Front/my-pollution-app/src/app/app.routes.ts
+++ Front/my-pollution-app/src/app/app.routes.ts
@@ -5,8 +5,6 @@ import { Store } from '@ngxs/store';
 import { PollutionListComponent } from './pollution-list/pollution-list.component';
 // ...existing code...
 import { PollutionRecapComponent } from './pollution-recap/pollution-recap.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { LoginComponent } from './login/login.component';
 import { AuthState } from './store/auth.state';
@@ -37,16 +35,6 @@ export const routes: Routes = [
   { 
     path: 'pollution/:id', 
     component: PollutionRecapComponent,
-    canActivate: [authGuard]
-  },
-  { 
-    path: 'utilisateur', 
-    component: UserListComponent,
-    canActivate: [authGuard]
-  },
-  { 
-    path: 'add-user', 
-    component: UserFormComponent,
     canActivate: [authGuard]
   },
 ];
